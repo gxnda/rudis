dfbedd076e7f8d801f1d429a9e1a3d489414e156
github-action-benchmark <github@users.noreply.github.com>
2026-08-05 16:51:02 +0000

    add Redis Clone Performance (customBiggerIsBetter) benchmark result for cb14c9862d8bdfd3904b994b85a539ce7260948c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785947600708,
+  "lastUpdate": 1785948662908,
   "repoUrl": "https://github.com/gxnda/rudis",
   "entries": {
     "Redis Clone Performance": [
@@ -33,6 +33,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "PING_MBULK",
             "value": 51247,
+            "unit": "undefined"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gabriellancasterwest@gmail.com",
+            "name": "Gabriel Lancaster-West",
+            "username": "gxnda"
+          },
+          "committer": {
+            "email": "gabriellancasterwest@gmail.com",
+            "name": "Gabriel Lancaster-West",
+            "username": "gxnda"
+          },
+          "distinct": true,
+          "id": "cb14c9862d8bdfd3904b994b85a539ce7260948c",
+          "message": "chore: replaced \\r with \\n in benchmark parsing",
+          "timestamp": "2026-08-05T17:49:47+01:00",
+          "tree_id": "6c0479722a77e05cbe82c2ba1cfd00b429e83ed4",
+          "url": "https://github.com/gxnda/rudis/commit/cb14c9862d8bdfd3904b994b85a539ce7260948c"
+        },
+        "date": 1785948662062,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "PING_INLINE",
+            "value": 58892.82,
+            "unit": "undefined"
+          },
+          {
+            "name": "PING_MBULK",
+            "value": 58754.41,
             "unit": "undefined"
           }
         ]
