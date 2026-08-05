b0c61e6d0afefe939b3b2dcfd3f1bc27527f1adf
github-action-benchmark <github@users.noreply.github.com>
2026-08-05 16:33:20 +0000

    add Redis Clone Performance (customBiggerIsBetter) benchmark result for 855c66cb2cd13ac7b873deb2a5ab6538b7b64f97

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1785947600708,
+  "repoUrl": "https://github.com/gxnda/rudis",
+  "entries": {
+    "Redis Clone Performance": [
+      {
+        "commit": {
+          "author": {
+            "email": "gabriellancasterwest@gmail.com",
+            "name": "Gabriel Lancaster-West",
+            "username": "gxnda"
+          },
+          "committer": {
+            "email": "gabriellancasterwest@gmail.com",
+            "name": "Gabriel Lancaster-West",
+            "username": "gxnda"
+          },
+          "distinct": true,
+          "id": "855c66cb2cd13ac7b873deb2a5ab6538b7b64f97",
+          "message": "lint: new line",
+          "timestamp": "2026-08-05T17:32:02+01:00",
+          "tree_id": "a9d0184f4df682b089889cf5214f86fee2942c71",
+          "url": "https://github.com/gxnda/rudis/commit/855c66cb2cd13ac7b873deb2a5ab6538b7b64f97"
+        },
+        "date": 1785947600069,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "PING_INLINE",
+            "value": 0,
+            "unit": "undefined"
+          },
+          {
+            "name": "PING_MBULK",
+            "value": 51247,
+            "unit": "undefined"
+          }
+        ]
+      }
+    ]
+  }
+}
