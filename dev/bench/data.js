50c79a81968bc50be06c772712971c7eb8e68094
Gabriel Lancaster-West <gabriellancasterwest@gmail.com>
2026-08-05 17:55:28 +0100

    fix: removed broken first data point

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -16,40 +16,6 @@ window.BENCHMARK_DATA = {
             "username": "gxnda"
           },
           "distinct": true,
-          "id": "855c66cb2cd13ac7b873deb2a5ab6538b7b64f97",
-          "message": "lint: new line",
-          "timestamp": "2026-08-05T17:32:02+01:00",
-          "tree_id": "a9d0184f4df682b089889cf5214f86fee2942c71",
-          "url": "https://github.com/gxnda/rudis/commit/855c66cb2cd13ac7b873deb2a5ab6538b7b64f97"
-        },
-        "date": 1785947600069,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "PING_INLINE",
-            "value": 0,
-            "unit": "undefined"
-          },
-          {
-            "name": "PING_MBULK",
-            "value": 51247,
-            "unit": "undefined"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "gabriellancasterwest@gmail.com",
-            "name": "Gabriel Lancaster-West",
-            "username": "gxnda"
-          },
-          "committer": {
-            "email": "gabriellancasterwest@gmail.com",
-            "name": "Gabriel Lancaster-West",
-            "username": "gxnda"
-          },
-          "distinct": true,
           "id": "cb14c9862d8bdfd3904b994b85a539ce7260948c",
           "message": "chore: replaced \\r with \\n in benchmark parsing",
           "timestamp": "2026-08-05T17:49:47+01:00",
