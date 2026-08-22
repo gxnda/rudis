7c7a56d9ac17c7584f86f47f05973683a204de29
Gabriel Lancaster-West <gabriellancasterwest@gmail.com>
2026-08-22 17:57:31 +0100

    fix: set coarsetime update to 100ms (was 2000ms)

--- src/server.rs
+++ src/server.rs
@@ -50,7 +50,7 @@ impl Server {
         storage: Arc<StorageEngine>,
     ) -> Result<(Self, oneshot::Sender<()>), PersistenceError> {
         let (shutdown_tx, shutdown_rx) = oneshot::channel();
-        Updater::new(2000).start()?;
+        Updater::new(100).start()?;
         Ok((
             Server {
                 listener: TcpListener::bind(config.addr).await?,
